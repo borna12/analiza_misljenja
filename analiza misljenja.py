b40d5b79b80497b4393891b827365a1ac9409ca0
Unknown <jomihx@gmail.com>
2018-08-14 17:03:09 +0200

    ajde

--- analiza misljenja.py
+++ analiza misljenja.py
@@ -5,27 +5,54 @@ from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import names
 import io
  
+def pozitivan():
+    positive_vocab=[]
+    for red in io.open("pozitivno.txt","r", encoding="utf-8-sig"):
+        red=red.strip("\n")
+        if red=="":
+            print ()
+        else:    
+            positive_vocab.append(str(red))
+    return positive_vocab
+
+def neutralan():   
+    neutral_vocab=[]
+    for red in io.open("neutralno.txt","r", encoding="utf-8-sig"):
+        red=red.strip("\n")
+        if red=="":
+            print ()
+        else:    
+            neutral_vocab.append(str(red))
+    return neutral_vocab
+
+def negativan(): 
+    negative_vocab=[]
+    for red in io.open("negativno.txt","r", encoding="utf-8-sig"):
+        red=red.strip("\n")
+        if red=="":
+            print ()
+        else:    
+            negative_vocab.append(str(red))
+    return negative_vocab
+
+
 def word_feats(words):
     return dict([(word, True) for word in words])
-#negative_vocab = [ 'loš', 'užasan','beskoristan', 'zločest', ':(', "tužan", "smrt","lijen"]
-#positive_vocab = [ 'dobar', 'dobro', 'izvrsno', 'pozitivno', 'pozitivan', 'odličan', 'solidan', ':)', 'trud', 'rad', 'lijep', 'predobar', 'prelijep', 'božanstven', 'Bog','koristan','lijep',"ljubav", "marljiv","voli"]
-#neutral_vocab = [ 'film','škola','osoba','zgrada','je','i','ali','pa','neka','ovaj','taj','to',"ići","u"]
-positive_vocab=[]
-neutral_vocab=[]
-negative_vocab=[]
-for red in io.open("pozitivno.txt","r+",encoding='utf8'):
-    red=red.strip("\n")
-    positive_vocab.append(str(red.encode("utf-8")).strip("b'"))
+
+"""
+negative_vocab = ['loš', 'užasan', 'beskoristan', 'zločest', 'zloča', 'zlo', ':(', 'tužan', 'smrt', 'opasnost', 'opasan', 'lijen', 'mrzim', 'mržnja', 'glupo', 'besmisleno', 'besmislen', 'besmislenost', 'složeno', 'nejasno', 'zbunjen', 'zbunjujuće', 'tužno', 'tužan', 'tuga', 'bol', 'bolestan', 'boležljiv', 'istrebljenje', 'rasizam', 'seksizam', 'mobing', 'zlostavljanje', 'droga', 'negativno', 'negativan', 'zbunjeno', 'nejasno', 'bol', 'bolest', 'uvreda', 'vrijeđati', 'omaložavanje', 'ignoriranje', 'sramota', 'sramoćenje', 'grozan', 'grozota', 'genocid', 'rat', 'borba', 'nasilje', 'loš', 'preloš', 'oružje']
+positive_vocab = [ 'dobar', 'dobro', 'izvrsno', 'pozitivno', 'pozitivan', 'odličan', 'solidan', ':)', 'trud', 'rad', 'lijep', 'predobar', 'prelijep', 'božanstven', 'Bog','koristan','lijep',"ljubav", "marljiv","voli"]
+neutral_vocab = ['film','škola','osoba','zgrada','je','i','ali','pa','neka','ovaj','taj','to',"ići","u"]
+"""
 
 
+positive_vocab=pozitivan()
+negative_vocab=negativan()
+neutral_vocab= neutralan()
 
-for red in io.open("neutralno.txt","r+",encoding='utf8'):
-    red=red.strip("\n")
-    neutral_vocab.append(str(red.encode("utf-8")).strip("b'"))
-
-for red in io.open("negativno.txt","r+",encoding='utf8'):
-    red=red.strip("\n")
-    negative_vocab.append(str(red.encode("utf-8")).strip("b'"))
+print (positive_vocab)
+print (negative_vocab)
+print (neutral_vocab)
 
 positive_features = [(word_feats(pos), 'pos') for pos in positive_vocab]
 negative_features = [(word_feats(neg), 'neg') for neg in negative_vocab]
@@ -35,6 +62,7 @@ def myfunction(*event):
         gumb.config(state='normal')
     else:
         gumb.config(state='disabled')
+
 
 def analiza():
     train_set = negative_features + positive_features + neutral_features
