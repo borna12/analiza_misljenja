649b6daa33cf1471fec7159a598929a37b18c369
Unknown <jomihx@gmail.com>
2018-10-09 10:56:39 +0200

    zam

--- sentiment.py
+++ sentiment.py
@@ -1,5 +1,6 @@
 from textblob import TextBlob
 from textblob.classifiers import NaiveBayesClassifier
+
 import io
 import sys
 import os
@@ -7,8 +8,8 @@ from os import path
 from tkinter import *
 from tkinter import ttk
 
-
-
+script_dir = os.path.dirname(__file__)
+file_path = os.path.join(script_dir, 'pozitivno.txt')
 
 
 
@@ -18,22 +19,34 @@ def myfunction(*event):
     else:
         gumb.config(state='disabled')
 
+def negativan(): 
+    for red in io.open(script_dir+"/negativno.txt","r", encoding="utf-8-sig"):
+        red=red.strip("\n").lower()
+        if red=="":
+            print ()
+        else:    
+            train.append((str(red),"neg"))
+
 def pozitivan():
-    for red in io.open("pozitivno.txt","r", encoding="utf-8-sig"):
+    for red in io.open(script_dir+"/pozitivno.txt","r", encoding="utf-8-sig"):
         red=red.strip("\n")
         if red=="":
             print ()
         else:    
             train.append((str(red),"poz"))
 
-
-def negativan(): 
-    for red in io.open("negativno.txt","r", encoding="utf-8-sig"):
-        red=red.strip("\n")
-        if red=="":
-            print ()
-        else:    
-            train.append((str(red),"neg"))
+def center(win):
+    win.update_idletasks()
+    width = win.winfo_width()
+    frm_width = win.winfo_rootx() - win.winfo_x()
+    win_width = width + 2 * frm_width
+    height = win.winfo_height()
+    titlebar_height = win.winfo_rooty() - win.winfo_y()
+    win_height = height + titlebar_height + frm_width
+    x = win.winfo_screenwidth() // 2 - win_width // 2
+    y = win.winfo_screenheight() // 2 - win_height // 2
+    win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+    win.deiconify() 
 
 
 train=[]
@@ -45,7 +58,9 @@ def analiza():
     cl = NaiveBayesClassifier(train)
     sentence = TextArea.get("1.0",END)
     #sentence = sentence.lower()
-    blob = TextBlob(sentence, classifier=cl)
+    blob = TextBlob(sentence.lower(), classifier=cl)
+    blob2 = TextBlob(sentence, classifier=cl)
+
     neg = 0
     pos = 0
     prozor2=Tk()
@@ -67,10 +82,10 @@ def analiza():
     tree.config(yscrollcommand=scrollbar.set) 
     scrollbar.config(command=tree.yview)
 
-    for s in blob.sentences:
-        print(s)
+    for s, s2 in zip(blob.sentences, blob2.sentences):
+        print(s2) 
         print (s.classify())
-        tree.insert('', 'end', text=s,values=s.classify())
+        tree.insert('', 'end', text=s2,values=s.classify())
         if (s.classify()=="poz"):
             pos+=1
         else:
@@ -96,5 +111,9 @@ gumb.config(state='disabled')
 prozor.bind_class("Text","<Leave>",myfunction)
 prozor.title("Analiza mišljenja")
 """prozor.iconbitmap(r"favicon.ico")"""
+
+
+center(prozor)
+
 prozor.resizable(False, False)
 prozor.mainloop()
